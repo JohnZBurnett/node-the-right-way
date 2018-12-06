e7aa5d9cf472b78d8d212e1fdc927d3abb67eda4
JohnZBurnett <tuninator@gmail.com>
2018-12-06 13:38:38 -0500

    add GET for ES to search.js

--- web-services/b4/lib/search.js
+++ web-services/b4/lib/search.js
@@ -11,6 +11,32 @@ module.exports = (app, es) => {
      * Example: /api/search/books/authors/Twain
      */
     app.get('/api/search/books/:field/:query', (req, res) => {
+        const esReqBody = {
+            size: 10,
+            query: {
+                match: {
+                    [req.params.field]: req.params.query
+                }
+            },
+        };
 
+        const options = { url, json: true, body: esReqBody}; 
+
+        request.get(options, (err, esRes, esResBody) => {
+            if (err) {
+                res.status(502).json({
+                    error: 'bad_gateway', 
+                    reason: err.code
+                }); 
+                return; 
+            }
+
+            if (esRes.statusCode !== 200) {
+                res.status(esRes.statusCode).json(esResBody); 
+                return; 
+            }
+
+            res.status(200).json(esResBody.hits.hits.map(({_source}) => _source)); 
+        }); 
     }); 
 }; 
