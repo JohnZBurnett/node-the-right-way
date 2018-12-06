bbb7b512b31a8b9dd6c7658d95b055468aad2ed0
JohnZBurnett <tuninator@gmail.com>
2018-12-06 13:30:39 -0500

    add app.get to search.js

--- web-services/b4/lib/search.js
+++ web-services/b4/lib/search.js
@@ -6,4 +6,11 @@
 const request = require('request'); 
 module.exports = (app, es) => {
     const url = `http://${es.host}:${es.port}/${es.books_index}/book/_search`; 
+    /**
+     * Search for books by matching a particular field value.
+     * Example: /api/search/books/authors/Twain
+     */
+    app.get('/api/search/books/:field/:query', (req, res) => {
+
+    }); 
 }; 
